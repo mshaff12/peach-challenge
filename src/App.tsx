305cb3fb9541cfb4d0595d7cd213a01a8516c0fd
mshaffer0308 <mshaffer0308@vt.edu>
2024-08-07 13:43:01 -0500

    Adding search and moving to SearchBar implementation to test fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import { useState } from "react";
 import MovieList from "./components/MovieList";
 import { Movie } from "./types";
+import { fetchMovies } from "./services/api";
+import MovieSearch from "./components/MovieSearch";
 
 const App = () => {
 
   const [movies, setMovies] = useState<Movie[]>([]);
+
+  const handleSearch = (query: string) => {
+    fetchMovies(query).then((response) => {
+      if (response.data.response === "True") {
+        setMovies(response.data.Search);
+      } else {
+        console.error(response.data.Error);
+      }
+    });
+  };
   
   return (
     <div className="App">
+      <MovieSearch onSearch={handleSearch} />
       <MovieList movies={movies} />
     </div>
   );
--- src/components/MovieSearch.tsx
+++ src/components/MovieSearch.tsx
@@ -1,4 +1,9 @@
-const MovieSearch = () => {
+interface MovieSearchProps {
+  onSearch: (query: string) => void;
+}
+
+const MovieSearch = ({ onSearch }: MovieSearchProps) => {
+    return (null);
 };
 
 export default MovieSearch;
