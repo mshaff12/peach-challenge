4fc876382f479bc54b7e17d888c28e972f8ee42a
mshaffer0308 <mshaffer0308@vt.edu>
2024-08-07 13:49:04 -0500

    Adding Formik handling for MovieSearch and verified valid network response from API in dev tools

--- src/components/MovieSearch.tsx
+++ src/components/MovieSearch.tsx
@@ -1,9 +1,36 @@
+import { Field, Form, Formik } from "formik";
+import { Button, TextField, Container } from '@mui/material';
+
 interface MovieSearchProps {
   onSearch: (query: string) => void;
 }
 
 const MovieSearch = ({ onSearch }: MovieSearchProps) => {
-    return (null);
+    return (
+        <Container>
+          <Formik
+            initialValues={{ query: '' }}
+            onSubmit={(values, { resetForm }) => {
+              onSearch(values.query);
+              resetForm();
+            }}
+          >
+            {() => (
+              <Form>
+                <Field
+                  name="query"
+                  as={TextField}
+                  label="Search"
+                  variant="outlined"
+                />
+                <Button type="submit" variant="contained" color="primary">
+                  Search
+                </Button>
+              </Form>
+            )}
+          </Formik>
+        </Container>
+      );
 };
 
 export default MovieSearch;
