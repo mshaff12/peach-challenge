b60efb92f30189b05f9191a156b6e5b7f8b77a64
mshaffer0308 <mshaffer0308@vt.edu>
2024-08-07 16:36:36 -0500

    Filling out MovieFavorites component and adding to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,30 @@ import { Movie } from "./types";
 import { fetchMovies } from "./services/api";
 import MovieSearch from "./components/MovieSearch";
 import MovieDetails from "./components/MovieDetails";
+import { Container, AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+import { styled } from '@mui/system';
+import MovieFavorites from "./components/MovieFavorites";
 
 const App = () => {
 
   const [movies, setMovies] = useState<Movie[]>([]);
   const [selectedMovie, setSelectedMovie] = useState<string | null>(null);
+  const [favorites, setFavorites] = useState<Movie[]>([]);
+  const [view, setView] = useState<'movies' | 'favorites'>('movies');
+
+  const StyledAppBar = styled(AppBar)(({ theme }) => ({
+    marginBottom: theme.spacing(4),
+  }));
+  
+  const HeaderButtons = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    gap: theme.spacing(2),
+    marginLeft: theme.spacing(2),
+  }));
+  
+  const MainContainer = styled(Container)(({ theme }) => ({
+    paddingTop: theme.spacing(4),
+  }));
 
   const handleSearch = (query: string) => {
     fetchMovies(query).then((response) => {
@@ -16,7 +35,6 @@ const App = () => {
         setMovies(response.data.Search);
       } else {
         console.error(response.data.Error); //TODO: Verify this is the response object format
-        console.log("test");
       }
     });
   };
@@ -24,16 +42,42 @@ const App = () => {
   const handleMovieClick = (movieId: string) => {
     setSelectedMovie(movieId);
   };
+
+  const handleRemoveFavorite = (id: string) => {
+    setFavorites(favorites.filter((movie) => movie.imdbID !== id));
+  };
+
+  const handleViewChange = (newView: 'movies' | 'favorites') => {
+    setView(newView);
+    setSelectedMovie(null);
+  };
   
   return (
-    <div className="App">
-      <MovieSearch onSearch={handleSearch} />
-      <MovieList movies={movies} onMovieClick={handleMovieClick} />
-      {selectedMovie && (
-        <MovieDetails movieId={selectedMovie} /> 
+    <MainContainer>
+      <StyledAppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>PEACHFLIX</Typography>
+          <HeaderButtons>
+            <Button variant="contained" color={view === 'movies' ? 'primary' : 'inherit'} onClick={() => handleViewChange('movies')}>
+              Movies
+            </Button>
+            <Button variant="contained" color={view === 'favorites' ? 'primary' : 'inherit'} onClick={() => handleViewChange('favorites')}>
+              Favorites
+            </Button>
+          </HeaderButtons>
+          <MovieSearch onSearch={handleSearch} />
+        </Toolbar>
+      </StyledAppBar>
+      {view === 'movies' ? (
+        <MovieList movies={movies} onMovieClick={handleMovieClick} />
+      ) : (
+        <MovieFavorites favorites={favorites} onRemoveFavorite={handleRemoveFavorite} />
       )}
-    </div>
+      {selectedMovie && (
+        <MovieDetails movieId={selectedMovie} />
+      )}
+    </MainContainer>
   );
-}
+};
 
 export default App;
--- src/components/MovieFavorites.tsx
+++ src/components/MovieFavorites.tsx
@@ -1,4 +1,33 @@
-const MovieFavorites = () => {
+import React from "react";
+import { Grid, Container } from "@mui/material";
+import { styled } from "@mui/system";
+import MovieItem from "./MovieItem";
+import { Movie } from "../types";
+
+interface FavoritesProps {
+  favorites: Movie[];
+  onRemoveFavorite: (id: string) => void;
+}
+
+const StyledContainer = styled(Container)(({ theme }) => ({
+  marginTop: theme.spacing(4),
+}));
+
+const Favorites = ({ favorites, onRemoveFavorite }: FavoritesProps) => {
+  return (
+    <StyledContainer>
+      <Grid container spacing={4}>
+        {favorites.map((movie) => (
+          <Grid item key={movie.imdbID} xs={12} sm={6} md={4} lg={3}>
+            <MovieItem
+              movie={movie}
+              onClick={() => onRemoveFavorite(movie.imdbID)}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </StyledContainer>
+  );
 };
 
-export default MovieFavorites;
+export default Favorites;
